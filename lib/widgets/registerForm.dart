3ddab2e59b4490ed313ee708994384d36d84f648
RazFernandez <miguelraz2002@gmail.com>
2024-10-10 20:15:18 -0500

    ✨feat: register form beta

--- lib/widgets/registerForm.dart
+++ lib/widgets/registerForm.dart
@@ -8,9 +8,29 @@ class RegisterForm extends StatefulWidget {
 }
 
 class _RegisterFormState extends State<RegisterForm> {
+  // The ID of this form
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return Form(
+        child: Column(
+      children: <Widget>[
+        // Here are the fields for this form
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: 'Nombres',
+            prefixIcon: Icon(Icons.person_2_outlined),
+          ),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Por favor, ingresa tu nombre';
+            }
+            return null;
+          },
+        ),
+      ],
+    ));
   }
 }
