9a5d9433506eb8b5f1e72a93cb61c07cd19203b7
RazFernandez <miguelraz2002@gmail.com>
2024-11-18 17:45:39 -0500

    ♻️refactor: Raname the customForms and move it to another folder

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:club_steam_app/models/user_model.dart';
 import 'package:club_steam_app/utils/Strings.dart';
 
 class AuthController {
