339da83c51a6cfdc4c746dc005491978c280d656
RazFernandez <mr.fernandez.mx@outlook.com>
2025-03-04 02:35:46 -0500

    🐛 bug: Add provider for state manage of the linear bar making it more reusable

--- lib/views/login_workflow/registration_email/register.dart
+++ lib/views/login_workflow/registration_email/register.dart
@@ -3,7 +3,8 @@ import 'package:club_steam_app/views/login_workflow/login.dart';
 import 'package:club_steam_app/widgets/Forms/registerForm.dart';
 import 'package:club_steam_app/widgets/Buttons/redirectTextButton.dart';
 import 'package:club_steam_app/widgets/Graphics/linearProgressBar.dart';
-import 'package:club_steam_app/controllers/progressBarController.dart';
+import 'package:club_steam_app/providers/linearProgressBar_provider.dart';
+import 'package:provider/provider.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -13,60 +14,63 @@ class RegisterView extends StatefulWidget {
 }
 
 class _RegisterViewState extends State<RegisterView> {
-  // Instance of the progress bar controller
-  // final ProgressBarController _progressBarController =
-  //     ProgressBarController(progress: 2.0, steps: 4.0);
-  // double xd = 2.0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Registro'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          // Keep progress bar at the top
-          LinearProgressBar.material(
-            steps: 2,
-            minHeight: 5.0,
-            context: context,
-          ),
-
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    RegisterForm(),
-                    SizedBox(height: 16),
-                    // Register Text
-                    FilledButton(
-                        onPressed: () {
-                          // Call the addStep method from the LinearProgressBar
-                          // _progressBarKey.addStep();
+      body: ChangeNotifierProvider(
+        create: (context) => LinearProgressBarProvider(2),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Keep progress bar at the top
+            LinearProgressBar.material(
+              minHeight: 5.0,
+              context: context,
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      RegisterForm(),
+                      SizedBox(height: 16),
+                      // Register Text
+                      Builder(
+                        builder: (context) {
+                          return FilledButton(
+                            onPressed: () {
+                              // Call the addStep method from the LinearProgressBar
+                              Provider.of<LinearProgressBarProvider>(context,
+                                      listen: false)
+                                  .incrementStep();
+                            },
+                            child: Text('Siguiente'),
+                          );
                         },
-                        child: Text('Siguiente')),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        RedirectTextButton(
-                          navigationMode: 1,
-                          labelText: "¿Ya tienes una cuenta?",
-                          context: context,
-                          targetView: LoginView(),
-                          buttonText: "Iniciar Sesión",
-                        ),
-                      ],
-                    ),
-                  ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          RedirectTextButton(
+                            navigationMode: 1,
+                            labelText: "¿Ya tienes una cuenta?",
+                            context: context,
+                            targetView: LoginView(),
+                            buttonText: "Iniciar Sesión",
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
