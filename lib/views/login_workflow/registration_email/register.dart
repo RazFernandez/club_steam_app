47bfeebacbc830e9676e793a560603591d502101
RazFernandez <mr.fernandez.mx@outlook.com>
2025-03-04 13:47:36 -0500

    🔨 pre fact: register frlow partially completed. fix UI and button to create the account

--- lib/views/login_workflow/registration_email/register.dart
+++ lib/views/login_workflow/registration_email/register.dart
@@ -5,6 +5,7 @@ import 'package:club_steam_app/widgets/Buttons/redirectTextButton.dart';
 import 'package:club_steam_app/widgets/Graphics/linearProgressBar.dart';
 import 'package:club_steam_app/providers/linearProgressBar_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:club_steam_app/widgets/Graphics/EditableAvatarImageProfile.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -14,6 +15,9 @@ class RegisterView extends StatefulWidget {
 }
 
 class _RegisterViewState extends State<RegisterView> {
+  // Index of the view to be displayed
+  int indexView = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,36 +38,48 @@ class _RegisterViewState extends State<RegisterView> {
               child: SingleChildScrollView(
                 child: Padding(
                   padding: EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      RegisterForm(),
-                      SizedBox(height: 16),
-                      // Register Text
-                      Builder(
-                        builder: (context) {
-                          return FilledButton(
-                            onPressed: () {
-                              // Call the addStep method from the LinearProgressBar
-                              Provider.of<LinearProgressBarProvider>(context,
-                                      listen: false)
-                                  .incrementStep();
-                            },
-                            child: Text('Siguiente'),
-                          );
-                        },
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                  child: IndexedStack(
+                    index: indexView,
+                    children: <Widget>[
+                      Column(
                         children: [
-                          RedirectTextButton(
-                            navigationMode: 1,
-                            labelText: "¿Ya tienes una cuenta?",
-                            context: context,
-                            targetView: LoginView(),
-                            buttonText: "Iniciar Sesión",
+                          RegisterForm(),
+                          SizedBox(height: 16),
+                          // Register Text
+                          Builder(
+                            builder: (context) {
+                              return FilledButton(
+                                onPressed: () {
+                                  // Call the addStep method from the LinearProgressBar
+                                  Provider.of<LinearProgressBarProvider>(
+                                          context,
+                                          listen: false)
+                                      .incrementStep();
+
+                                  setState(() {
+                                    indexView = 1;
+                                  });
+                                },
+                                child: Text('Siguiente'),
+                              );
+                            },
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              RedirectTextButton(
+                                navigationMode: 1,
+                                labelText: "¿Ya tienes una cuenta?",
+                                context: context,
+                                targetView: LoginView(),
+                                buttonText: "Iniciar Sesión",
+                              ),
+                            ],
                           ),
                         ],
                       ),
+                      Editableavatarimageprofile(),
+                      SizedBox(height: 16),
                     ],
                   ),
                 ),
